d62352d26588428e4e7d2fef671566ac79041566
Nicos Angelopoulos <nicos.angelopoulos@gmail.com>
2018-09-22 13:33:53 +0100

    VER:  1.0 and slight change on pack description.

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(pub_graph ).
-title('Access, cache and visualise citation relations in pubmed records').
-version('0.1').
+title('Access, cache and visualise citation relations in publications servers').
+version('1.0').
 author('Nicos Angelopoulos', 'http://stoics.org.uk/~nicos').
 packager('Nicos Angelopoulos', 'http://stoics.org.uk/~nicos').
 maintainer('Nicos Angelopoulos', 'http://stoics.org.uk/~nicos').
